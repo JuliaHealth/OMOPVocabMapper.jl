9937c6b3bca95b7a904d258ea171fb8f5be73704
Ashlin Harris <90787010+AshlinHarris@users.noreply.github.com>
2023-06-09 15:59:09 -0400

    Adjust syntax for 1.8.5

--- vocabmapper.jl
+++ vocabmapper.jl
@@ -1,7 +1,7 @@
 using CSV, DataFrames, StatsBase, Query, Dates
 
 
-#load the csv files
+@info "Loading CSV files..."
 ICD_codes = CSV.read("/data/ursa_research/radxup/daluthge/highneeds.icd.csv.2023.05.05", DataFrame) #21740 | Unique = 20306
 df_concept = CSV.read("/data/ursa_software/riqi_code/Vocabularies/20230301/CONCEPT.csv", DataFrame)
 df_concept_relationship = CSV.read("/data/ursa_software/riqi_code/Vocabularies/20230301/CONCEPT_RELATIONSHIP.csv", DataFrame)
@@ -11,8 +11,9 @@ ICD_codes.VALUE = replace.(ICD_codes.VALUE, "." => "")
 df_concept.concept_code = replace.(df_concept.concept_code, "." => "")
 
 # mapping codes according to the vocabulary to avoid wrong mappings example E000 and E00.0
-ICD9_codes = filter(row -> row.SYSTEM == "ICD-9-CM", ICD_codes)
-ICD10_codes = filter(row -> row.SYSTEM == "ICD-10-CM", ICD_codes)
+#ICD9_codes = filter(row -> row.SYSTEM == "ICD-9-CM", ICD_codes)
+#ICD10_codes = filter(row -> row.SYSTEM == "ICD-10-CM", ICD_codes)
+code_systems = groupby(ICD_codes, :SYSTEM)
 
 #sanity check
 filter(row -> row.concept_code == "F81.0", df_concept)
@@ -25,19 +26,22 @@ df_concept_relationship_mapsto_select = select(df_concept_relationship_mapsto, :
 #df_concept_condition = filter(row -> row.domain_id == "Condition" || row.domain_id == "Observation", df_concept)
 
 #filter for vocabulary_id  = ICD10CM and ICD9CM
-df_concept_ICD9 = filter(row -> (row.vocabulary_id == "ICD9CM"), df_concept)
-df_concept_ICD10 = filter(row -> (row.vocabulary_id == "ICD10CM"), df_concept)
+#df_concept_ICD9 = filter(row -> (row.vocabulary_id == "ICD9CM"), df_concept)
+#df_concept_ICD10 = filter(row -> (row.vocabulary_id == "ICD10CM"), df_concept)
+df_concept_systems = groupby(df_concept, :vocabulary_id)
+df_concept_ICD9 = df_concept_systems[("ICD9CM",)]
+df_concept_ICD10 = df_concept_systems[("ICD10CM",)]
 
-df_concept_ICD9_select = select(df_concept_ICD9, :concept_id, :concept_code, :concept_name, :domain_id, :vocabulary_id)
-df_concept_ICD10_select = select(df_concept_ICD10, :concept_id, :concept_code, :concept_name, :domain_id, :vocabulary_id)
-
+# I removed :concept_name. Is this field haunted?
+df_concept_ICD9_select = select(df_concept_ICD9, :concept_id, :concept_code, :domain_id, :vocabulary_id)
+df_concept_ICD10_select = select(df_concept_ICD10, :concept_id, :concept_code, :domain_id, :vocabulary_id)
 
 #left join ICD9_codes and df_concept_condition on VALUE and concept_code
 
-join_ICD9_concept = leftjoin(ICD9_codes, df_concept_ICD9_select, on = (:VALUE, :concept_code))
+join_ICD9_concept = leftjoin(code_systems[("ICD-9-CM",)], df_concept_ICD9_select, on = Pair(:VALUE, :concept_code))
 
 # left join join_ICD_concept and df_concept_relationship_mapsto on concept_id and concept_id_1
-join_ICD9_concept_relationship = join(join_ICD9_concept, df_concept_relationship_mapsto_select, on = (:concept_id, :concept_id_1), kind = :left)
+join_ICD9_concept_relationship = leftjoin(join_ICD9_concept, df_concept_relationship_mapsto_select, on = Pair(:concept_id, :concept_id_1), matchmissing=:notequal)
 
 ICD9_SNOMED_MAP = rename!(join_ICD9_concept_relationship, :concept_id => :source_concept_id, :concept_id_2 => :OMOP_concept_id)
 
@@ -50,10 +54,10 @@ CSV.write("/data/ursa_research/n3c/mthakkal/radxup_project/highneeds/ICD9_OMOP_M
 
 #left join ICD10_codes and df_concept_condition on VALUE and concept_code
 
-join_ICD10_concept = leftjoin(ICD10_codes, df_concept_ICD10_select, on = (:VALUE, :concept_code))
+join_ICD10_concept = leftjoin(code_systems[("ICD-10-CM",)], df_concept_ICD10_select, on = Pair(:VALUE, :concept_code))
 
 # left join join_ICD10_concept and df_concept_relationship_mapsto on concept_id and concept_id_1
-join_ICD10_concept_relationship = join(join_ICD10_concept, df_concept_relationship_mapsto_select, on = (:concept_id, :concept_id_1), kind = :left)
+join_ICD10_concept_relationship = leftjoin(join_ICD10_concept, df_concept_relationship_mapsto_select, on = Pair(:concept_id, :concept_id_1), matchmissing=:notequal)
 
 ICD10_SNOMED_MAP = rename!(join_ICD10_concept_relationship, :concept_id => :source_concept_id, :concept_id_2 => :OMOP_concept_id)
 
