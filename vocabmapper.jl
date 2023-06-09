c685655045de6ced6c65b46867692ee3fe396b37
Ashlin Harris <90787010+AshlinHarris@users.noreply.github.com>
2023-06-09 16:07:11 -0400

    Add info messages

--- vocabmapper.jl
+++ vocabmapper.jl
@@ -1,22 +1,21 @@
+@info "Loading packages..."
 using CSV, DataFrames, StatsBase, Query, Dates
-
 
 @info "Loading CSV files..."
 ICD_codes = CSV.read("/data/ursa_research/radxup/daluthge/highneeds.icd.csv.2023.05.05", DataFrame) #21740 | Unique = 20306
 df_concept = CSV.read("/data/ursa_software/riqi_code/Vocabularies/20230301/CONCEPT.csv", DataFrame)
 df_concept_relationship = CSV.read("/data/ursa_software/riqi_code/Vocabularies/20230301/CONCEPT_RELATIONSHIP.csv", DataFrame)
 
+@info "Mapping codes..."
 # Remove decimal points from the ICD codes
 ICD_codes.VALUE = replace.(ICD_codes.VALUE, "." => "")
 df_concept.concept_code = replace.(df_concept.concept_code, "." => "")
 
 # mapping codes according to the vocabulary to avoid wrong mappings example E000 and E00.0
-#ICD9_codes = filter(row -> row.SYSTEM == "ICD-9-CM", ICD_codes)
-#ICD10_codes = filter(row -> row.SYSTEM == "ICD-10-CM", ICD_codes)
 code_systems = groupby(ICD_codes, :SYSTEM)
 
 #sanity check
-filter(row -> row.concept_code == "F81.0", df_concept)
+@assert isempty(filter(row -> row.concept_code == "F81.0", df_concept))
 
 #filter maps to relationship
 df_concept_relationship_mapsto = filter(row -> row.relationship_id == "Maps to", df_concept_relationship)
@@ -26,8 +25,6 @@ df_concept_relationship_mapsto_select = select(df_concept_relationship_mapsto, :
 #df_concept_condition = filter(row -> row.domain_id == "Condition" || row.domain_id == "Observation", df_concept)
 
 #filter for vocabulary_id  = ICD10CM and ICD9CM
-#df_concept_ICD9 = filter(row -> (row.vocabulary_id == "ICD9CM"), df_concept)
-#df_concept_ICD10 = filter(row -> (row.vocabulary_id == "ICD10CM"), df_concept)
 df_concept_systems = groupby(df_concept, :vocabulary_id)
 df_concept_ICD9 = df_concept_systems[("ICD9CM",)]
 df_concept_ICD10 = df_concept_systems[("ICD10CM",)]
@@ -47,10 +44,11 @@ join_ICD10_concept_relationship = leftjoin(join_ICD10_concept, df_concept_relati
 ICD9_SNOMED_MAP = rename!(join_ICD9_concept_relationship, :concept_id => :source_concept_id, :concept_id_2 => :OMOP_concept_id)
 ICD10_SNOMED_MAP = rename!(join_ICD10_concept_relationship, :concept_id => :source_concept_id, :concept_id_2 => :OMOP_concept_id)
 
-# Checks
+@info "Verifying results..."
 @assert count(ismissing,ICD9_SNOMED_MAP.OMOP_concept_id) == 4
 @assert count(ismissing,ICD10_SNOMED_MAP.OMOP_concept_id) == 3
 
+@info "Exporting results..."
 CSV.write("/data/ursa_research/n3c/mthakkal/radxup_project/highneeds/ICD9_OMOP_MAP.csv", ICD9_SNOMED_MAP)
 CSV.write("/data/ursa_research/n3c/mthakkal/radxup_project/highneeds/ICD10_OMOP_MAP.csv", ICD10_SNOMED_MAP)
 
