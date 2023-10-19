044a7cc90f34d7378420ccb3980aada8e468b70d
Mounika Thakkallapally <86686601+Mounika-Thakkallapally@users.noreply.github.com>
2023-10-19 09:39:40 -0400

    Update vocabmapper.jl

--- vocabmapper.jl
+++ vocabmapper.jl
@@ -1,69 +1,104 @@
-@info "Loading packages..."
-using CSV
-using DataFrames
-using StatsBase
-using Query
-using Dates
+using CSV, DataFrames, StatsBase, Query, Dates
 
-@info "Loading CSV files..."
-ICD_codes = CSV.read("/data/ursa_research/radxup/daluthge/highneeds.icd.csv.2023.05.05", DataFrame) #21740 | Unique = 20306
-df_concept = CSV.read("/data/ursa_software/riqi_code/Vocabularies/20230301/CONCEPT.csv", DataFrame)
-df_concept_relationship = CSV.read("/data/ursa_software/riqi_code/Vocabularies/20230301/CONCEPT_RELATIONSHIP.csv", DataFrame)
+println("Enter directory for current ICD codes (/Users/mounikathakkallapally/Desktop/Brown/OmopVocabMapper/??):")
+# current dataset: breast_cancer
+working_drive = readline(stdin)
+cd("/Users/mounikathakkallapally/Desktop/Brown/OmopVocabMapper/$working_drive")
 
-# Show description of DataFrame, including number of unique values in each row
-function fully_describe(DF)
-	q = describe(DF)
-	q.num_unique_vals = map(x->length(DF[!,x] |> unique), names(DF))
-	q
-end
-println()
-println("Summary of ICD Codes:")
-show(
-	fully_describe(ICD_codes),
-	allrows=true, allcols=true
-) 
-println()
-println()
+#load the csv files
+ICD_codes = CSV.read("icd_codes.CSV", DataFrame)
 
-@info "Mapping codes..."
-# Remove decimal points from the ICD codes
-ICD_codes.VALUE = replace.(ICD_codes.VALUE, "." => "")
+ICD_codes.VALUE = replace.(ICD_codes.ICD, "." => "")
+
+ICD9_codes = filter(row -> row.system == "ICD9CM", ICD_codes)
+
+ICD10_codes = filter(row -> row.system == "ICD10CM", ICD_codes)
+
+df_concept = CSV.read("/Users/mounikathakkallapally/Desktop/Brown/user/mthakkal/Vocabularies/CONCEPT.CSV", DataFrame)
+
+df_concept_relationship = CSV.read("/Users/mounikathakkallapally/Desktop/Brown/user/mthakkal/Vocabularies/CONCEPT_RELATIONSHIP.csv", DataFrame)
+
+# Remove decimal points from the ICD-10 code column
 df_concept.concept_code = replace.(df_concept.concept_code, "." => "")
-@assert isempty(filter(x->contains(x.VALUE, "."), ICD_codes))
 
-# mapping codes according to the vocabulary to avoid wrong mappings example E000 and E00.0
-code_systems = groupby(ICD_codes, :SYSTEM)
-df_concept_relationship_mapsto = filter(row -> row.relationship_id == "Maps to", df_concept_relationship)
-df_concept_relationship_mapsto_select = select(df_concept_relationship_mapsto, :concept_id_1, :concept_id_2)
+#sanity check
+filter(row -> row.concept_code == "F81.0", df_concept)
+
+#filter maps to relationship
+df_concept_relationship_mapsto = filter(row -> (row.relationship_id == "Maps to"|| row.relationship_id == "Maps to value"), df_concept_relationship)
+
+# sanity check
+unique(df_concept_relationship_mapsto.relationship_id)
+
+df_concept_relationship_mapsto_select = select(df_concept_relationship_mapsto, :concept_id_1, :concept_id_2, :relationship_id)
 
 #filter the concept.csv for domain = condition
 #df_concept_condition = filter(row -> row.domain_id == "Condition" || row.domain_id == "Observation", df_concept)
 
 #filter for vocabulary_id  = ICD10CM and ICD9CM
-# I removed :concept_name. Is this field haunted?
-df_concept_systems = groupby(df_concept, :vocabulary_id)
-df_concept_ICD9_select = select(df_concept_systems[("ICD9CM",)], :concept_id, :concept_code, :domain_id, :vocabulary_id)
-df_concept_ICD10_select = select(df_concept_systems[("ICD10CM",)], :concept_id, :concept_code, :domain_id, :vocabulary_id)
+df_concept_ICD9 = filter(row -> (row.vocabulary_id == "ICD9CM"), df_concept)
 
-@info "Combining tables..."
-#left join ICD codes and df_concept_condition on VALUE and concept_code
-join_ICD9_concept = leftjoin(code_systems[("ICD-9-CM",)], df_concept_ICD9_select, on = Pair(:VALUE, :concept_code))
-join_ICD10_concept = leftjoin(code_systems[("ICD-10-CM",)], df_concept_ICD10_select, on = Pair(:VALUE, :concept_code))
+df_concept_ICD10 = filter(row -> (row.vocabulary_id == "ICD10CM"), df_concept)
+
+df_concept_ICD9_select = select(df_concept_ICD9, :concept_id, :concept_code, :concept_name, :domain_id, :vocabulary_id)
+
+rename!(df_concept_ICD9_select, :concept_name => :source_concept_name, :domain_id => :source_domain_id, :vocabulary_id => :source_vocabulary_id)
+
+df_concept_ICD10_select = select(df_concept_ICD10, :concept_id, :concept_code, :concept_name, :domain_id, :vocabulary_id)
+
+rename!(df_concept_ICD10_select, :concept_name => :source_concept_name, :domain_id => :source_domain_id, :vocabulary_id => :source_vocabulary_id)
+
+
+#left join ICD9_codes and with the concept table on VALUE and concept_code
+
+join_ICD9_concept = leftjoin(ICD9_codes, df_concept_ICD9_select, on = ( :VALUE => :concept_code))
+
 
 # left join join_ICD_concept and df_concept_relationship_mapsto on concept_id and concept_id_1
-join_ICD9_concept_relationship = leftjoin(join_ICD9_concept, df_concept_relationship_mapsto_select, on = Pair(:concept_id, :concept_id_1), matchmissing=:notequal)
-join_ICD10_concept_relationship = leftjoin(join_ICD10_concept, df_concept_relationship_mapsto_select, on = Pair(:concept_id, :concept_id_1), matchmissing=:notequal)
+join_ICD9_concept_relationship = leftjoin(join_ICD9_concept, df_concept_relationship_mapsto_select, on = (:concept_id => :concept_id_1))
 
-ICD9_SNOMED_MAP = rename!(join_ICD9_concept_relationship, :concept_id => :source_concept_id, :concept_id_2 => :OMOP_concept_id)
-ICD10_SNOMED_MAP = rename!(join_ICD10_concept_relationship, :concept_id => :source_concept_id, :concept_id_2 => :OMOP_concept_id)
+ICD9_SNOMED_MAP = rename(join_ICD9_concept_relationship, :concept_id => :source_concept_id, :concept_id_2 => :omop_concept_id)
 
-@info "Verifying results..."
-@assert count(ismissing,ICD9_SNOMED_MAP.OMOP_concept_id) == 4
-@assert count(ismissing,ICD10_SNOMED_MAP.OMOP_concept_id) == 3
+#adding the concept name and domain id for the standard omop concept id---- for doing this join the above dataframe with the concept table on omop_concept_id and concept_id in concept table
 
-@info "Exporting results..."
-CSV.write("/data/ursa_research/n3c/OmopVocabMapper/ICD9_OMOP_MAP.csv", ICD9_SNOMED_MAP)
-CSV.write("/data/ursa_research/n3c/OmopVocabMapper/ICD10_OMOP_MAP.csv", ICD10_SNOMED_MAP)
+icd9_omop_standard = leftjoin(ICD9_SNOMED_MAP, df_concept, on = (:omop_concept_id => :concept_id))
+
+select!(icd9_omop_standard, Not(:concept_code))
+#missing 4
+count(ismissing,icd9_omop_standard.omop_concept_id)
+
+
+#CSV.write("/data/ursa_research/n3c/mthakkal/radxup_project/highneeds/ICD_OMOP_codes/ICD9_OMOP_MAP.csv", icd9_omop_standard)
+
+
+#left join ICD10_codes and df_concept_condition on VALUE and concept_code
+
+join_ICD10_concept = leftjoin(ICD10_codes, df_concept_ICD10_select, on = (:VALUE => :concept_code))
+
+# left join join_ICD10_concept and df_concept_relationship_mapsto on concept_id and concept_id_1
+join_ICD10_concept_relationship = leftjoin(join_ICD10_concept, df_concept_relationship_mapsto_select, on = (:concept_id => :concept_id_1))
+
+ICD10_SNOMED_MAP = rename(join_ICD10_concept_relationship, :concept_id => :source_concept_id, :concept_id_2 => :omop_concept_id)
+
+#adding the concept name and domain id for the standard omop concept id---- for doing this join the above dataframe with the concept table on omop_concept_id and concept_id in concept table
+
+icd10_omop_standard = leftjoin(ICD10_SNOMED_MAP, df_concept, on = (:omop_concept_id => :concept_id))
+select!(icd10_omop_standard, Not(:concept_code))
+
+#missing 3
+count(ismissing,icd10_omop_standard.omop_concept_id)
+
+
+#CSV.write("/data/ursa_research/n3c/mthakkal/radxup_project/highneeds/ICD_OMOP_codes/ICD10_OMOP_MAP.csv", icd10_omop_standard)
+
+test_filter = filter(row -> row.concept_id_2 == join_ICD9_concept.concept_id, df_concept_relationship_mapsto_select)
+
+#combine both files
+
+combined = vcat(icd9_omop_standard, icd10_omop_standard)
+
+CSV.write("/Users/mounikathakkallapally/Desktop/Brown/OmopVocabMapper/$working_drive/omopmappedcodes_$working_drive.CSV", combined)
+
 
 #/data/ursa_software/riqi_code/Vocabularies/20230301/ ---- vocab files
 
@@ -72,3 +107,6 @@ CSV.write("/data/ursa_research/n3c/OmopVocabMapper/ICD10_OMOP_MAP.csv", ICD10_SN
 # JOIN cdm.concept c ON c.concept_id = cr.concept_id_1
 # WHERE cr.relationship_id = 'Maps to' -- Use 'Maps to' relationship for ICD-10 to SNOMED mapping
 #   AND c.concept_code in ('F81.0','F81.2', 'F81.8', '315.0', '315.00')
+
+
+
